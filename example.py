71a331af1b11c7337a7b319ea646fe6ddea5eaf0
soheepark96 <kosp2595@naver.com>
2019-04-04 22:50:17 +0900

    first commit

---
+++ example.py
@@ -0,0 +1,33 @@
+# print("Hello World!")
+# 변수 = input("입력 : ")
+# a = "asdasd"
+# print(변수) 
+#주석입니다.
+
+#리스트는 대괄호를 사용
+# a = (1,2,3)
+# print(a[1])
+# a[1] = 10 list에선 가능 tuple에선 에러가 뜸
+# print(a[1])
+# print('-'*30)
+# for b in a:
+#     print(b)
+
+# my_dic = {1:'자동차', 2:'냉장고', 3:'휴대폰'}
+# print(my_dic[1])
+# print(my_dic[2])
+# print(my_dic[3])
+# print(my_dic)
+
+# diction = {'a':1, 'b':2, 'c':3}
+# print(diction)
+# for a in diction:
+#     print(a)
+# print('-'*30)
+# for c in diction:
+#     print(diction[c])
+
+a = '1 2 3 4 5 6 7 8 9 10'
+print(a)
+b = a.split(' ')
+print(b)
